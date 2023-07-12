14c4d606ce8be9d98e1424baccb8b58995f4a6b3
Aurelio Maurici <maurici.aurelio@gmail.com>
2023-07-12 15:35:35 +0200

    img clickable instead of "voir projet" link

--- src/components/portfolio/PortfolioItem.jsx
+++ src/components/portfolio/PortfolioItem.jsx
@@ -1,62 +1,72 @@
-import React, {useEffect, useRef} from 'react'
-import MetaPost from "../header/MetaPost";
-import NavLink from "../button/NavLink";
-import BgImage from "../header/BgImage";
-import {getPortfolioLink} from "../../api/portfolio/PortfolioData";
-import {dsnCN} from "../../hooks/helper";
+import React, { useEffect, useRef } from 'react'
+import MetaPost from '../header/MetaPost'
+import NavLink from '../button/NavLink'
+import BgImage from '../header/BgImage'
+import { getPortfolioLink } from '../../api/portfolio/PortfolioData'
+import { dsnCN } from '../../hooks/helper'
 
+function PortfolioItem({ portoDetails, textButton, imageOnly, className }) {
+  const ref = useRef()
 
-function PortfolioItem({portoDetails, textButton, imageOnly, className}) {
+  useEffect(() => {
+    const video = ref.current.querySelector('video')
+    if (!video) return
 
-    const ref = useRef();
+    video.pause()
+    const portItem = ref.current,
+      mouseEnter = () => video.play(),
+      mouseLeve = () => video.pause()
 
-    useEffect(() => {
+    portItem.addEventListener('mouseenter', mouseEnter)
+    portItem.addEventListener('mouseleave', mouseLeve)
 
-        const video = ref.current.querySelector('video');
-        if (!video)
-            return;
+    return () => {
+      if (!video) return
+      portItem.removeEventListener('mouseenter', mouseEnter)
+      portItem.removeEventListener('mouseleave', mouseLeve)
+    }
+  }) // eslint-disable-line react-hooks/exhaustive-deps
 
-        video.pause();
-        const portItem = ref.current,
-            mouseEnter = () => video.play(),
-            mouseLeve = () => video.pause();
-
-
-        portItem.addEventListener('mouseenter', mouseEnter);
-        portItem.addEventListener('mouseleave', mouseLeve);
-
-        return () => {
-            if (!video)
-                return;
-            portItem.removeEventListener('mouseenter', mouseEnter);
-            portItem.removeEventListener('mouseleave', mouseLeve);
-        }
-
-    });// eslint-disable-line react-hooks/exhaustive-deps
-
-
-    return (
-        <div className={dsnCN('portfolio-item text-center v-dark-head', className)} ref={ref}>
-            <div className="item-border"/>
-            <div className='inner-img'>
-                <BgImage className={"dsn-swiper-parallax-transform"} src={portoDetails.src} video={portoDetails.video}
-                         alt={portoDetails.title}
-                         overlay={portoDetails.overlay} height={"100%"} imageOnly={imageOnly}/>
-
-            </div>
-            <div className="info-text">
-                {portoDetails.category && <MetaPost category={portoDetails.category} separate=", "/>}
-
-                {portoDetails.title &&
-                <h4 className="title-block"><NavLink to={getPortfolioLink(portoDetails)}>{portoDetails.title}</NavLink>
-                </h4>}
-
-                <NavLink className="btn-portfolio" to={getPortfolioLink(portoDetails)}>{textButton}</NavLink>
-            </div>
+  return (
+    <NavLink
+      className='btn-portfolio'
+      to={getPortfolioLink(portoDetails)}
+    >
+      <div
+        className={dsnCN('portfolio-item text-center v-dark-head', className)}
+        ref={ref}
+      >
+        <div className='item-border' />
+        <div className='inner-img'>
+          <BgImage
+            className={'dsn-swiper-parallax-transform'}
+            src={portoDetails.src}
+            video={portoDetails.video}
+            alt={portoDetails.title}
+            overlay={portoDetails.overlay}
+            height={'100%'}
+            imageOnly={imageOnly}
+          />
         </div>
+        <div className='info-text'>
+          {portoDetails.category && (
+            <MetaPost
+              category={portoDetails.category}
+              separate=', '
+            />
+          )}
 
-    )
+          {portoDetails.title && (
+            <h4 className='title-block'>
+              <NavLink to={getPortfolioLink(portoDetails)}>
+                {portoDetails.title}
+              </NavLink>
+            </h4>
+          )}
+        </div>
+      </div>
+    </NavLink>
+  )
 }
 
-
-export default React.memo(PortfolioItem);
+export default React.memo(PortfolioItem)
